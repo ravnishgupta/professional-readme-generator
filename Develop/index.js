cf698eda23cc51bb39c0168764ea0749fdfc0987
ravnishgupta <ravnish@gmail.com>
2022-04-05 22:55:37 -1000

    using node-fetch to call license API on github

--- Develop/index.js
+++ Develop/index.js
@@ -1,24 +1,12 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
+const fetch = require('node-fetch');
 const inquirer = require('inquirer');
 
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
 
-// TODO: Create an array of questions for user input
 const questions = [
     'What is the title of your project?',
-    'Please provide a short description explaining the what, why, and how of your project.',
+    'Please provide a short description of your project.',
     'Please provide installation instructions, if any.',
     'Please provide usage information, if any',
     'Please contribution guidelines, if any.',
@@ -28,11 +16,30 @@ const questions = [
     'What is your email address?'
 ];
 
+
+const getLicenses = () => {
+  const allLicenses = [];
+  const licenseAPI = 'https://api.github.com/licenses';
+  fetch(licenseAPI)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      //console.log(data);
+      for (var i=0; i<data.length; i++) {
+        allLicenses.push(data[i].name);
+      }
+    })
+    return allLicenses;
+}; 
+
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
 const init = () => {
+    
+
         return inquirer.prompt([
         {
           type: 'input',
@@ -86,11 +93,11 @@ const init = () => {
           type: 'list',
           name: 'license',
           message: questions[6], 
-          choices: ['MIT License (https://choosealicense.com/licenses/mit/)', 'GNU General Public License v3.0 (https://choosealicense.com/licenses/gpl-3.0/)']
+          choices: getLicenses()
         },
         {
           type: 'input',
-          name: 'githut',
+          name: 'github',
           message: questions[7], 
           validate: projectInput => {
             if (projectInput) {
@@ -110,44 +117,15 @@ const init = () => {
         }
       ])
       .then(answers => {
-        console.info('Answers:', answers);
+        const fileData = `# ${answers.projectTitle}\n ## Description\n ${answers.projectDesc}\n ## Installation\n ${answers.installInst} ## Usage\n ${answers.usageinfo}\n  ## Contributing\n ${answers.contribution}\n ## Tests\n ${answers.testInst}\n`
+       // fileData += `## License\n ${answers.license}\n ## Questions [github/${answers.github}](https://github.com/${answers.github})`
+       // console.log(fileData);
+
+        //[github/ravnishgupta](https://github.com/ravnishgupta)
       });
 }
 
 // Function call to initialize app
 init();
-
-
-// inquirer
-//   .prompt([
-//     {
-//       type: 'input',
-//       name: 'name',
-//       message: 'What is your name?'
-//     },
-//     {
-//       type: 'checkbox',
-//       message: 'What languages do you know?',
-//       name: 'stack',
-//       choices: ['HTML', 'CSS', 'JavaScript', 'MySQL']
-//     },
-//     {
-//       type: 'list',
-//       message: 'What is your preferred method of communication?',
-//       name: 'contact',
-//       choices: ['email', 'phone', 'telekinesis']
-//     }
-//   ])
-//   .then(data => {
-//     const filename = `${data.name
-//       .toLowerCase()
-//       .split(' ')
-//       .join('')}.json`;
-
-//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), err =>
-//       err ? console.log(err) : console.log('Success!')
-//     );
-//   });
-
 
 
